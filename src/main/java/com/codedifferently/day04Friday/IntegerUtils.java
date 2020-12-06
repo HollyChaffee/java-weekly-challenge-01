52a8b87b78ad464c6091d5f18c96f306a72512de
Holly Chaffee <saltdc95@gmail.com>
2020-12-06 17:42:54 -0500

    IntegerUtils working

--- src/main/java/com/codedifferently/day04Friday/IntegerUtils.java
+++ src/main/java/com/codedifferently/day04Friday/IntegerUtils.java
@@ -1,20 +1,33 @@
 package com.codedifferently.day04Friday;
 
 public class IntegerUtils {
+
     /**
      * @param n integer value input by client
      * @return the sum of all integers between 0 and not including `n`
      */
     public static Integer getSumOfN(Integer n) {
-        return null;
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum = sum + i;
+        }
+        return sum;
     }
+
 
     /**
      * @param val integer value input by client
      * @return integer with identical digits in the reverse order
      */
     public static Integer reverseDigits(Integer val) {
-        return null;
+        int reverseNum = 0;
+        while(val > 0)
+        {
+            reverseNum = (reverseNum * 10) + (val % 10);
+            val = val / 10;
+        }
+        return reverseNum;
     }
 
     /**
@@ -22,6 +35,12 @@ public class IntegerUtils {
      * @return true if the integer is even and false if it is not even.
      */
     public static Boolean isEven(Integer val) {
-        return null;
+        if (val % 2 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
     }
 }
